83d2bf97b124f6aae05006f891c5d5c59d0228ab
Vladislav <vlad.zheltov1@gmail.com>
2021-09-28 22:47:47 +0300

    Add about page

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import { Layout } from "@/layouts/Layout";
+import { Text } from "@/components/UI";
+
+const About = () => {
+    return (
+        <Layout>
+            <Text mode="h1">Страница о продукте</Text>
+        </Layout>
+    )
+}
+
+export default About;
