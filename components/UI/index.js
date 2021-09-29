67cf4af958d56d1cafbcfcade28161cb68454869
Vladislav <vlad.zheltov1@gmail.com>
2021-09-29 22:34:49 +0300

    Add Input component to the UI-kit

--- components/UI/index.js
+++ components/UI/index.js
@@ -1,12 +1,14 @@
 import { Button } from "./Button";
 import { Icon } from "./Icon";
-import { Text } from "./Text";
+import { Input } from "./Input";
 import { Space } from "./Space";
+import { Text } from "./Text";
 
 export {
     Button,
     Icon,
+    Input,
+    Space,
     Text,
-    Space
 };
 
--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,6 @@ export default function Home() {
                     </>
                 }
             />
-
         </Layout>
     )
 }
