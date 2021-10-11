4ea7f9828bac6247ab04caf09496d8cf0ced7883
Vladislav <vlad.zheltov1@gmail.com>
2021-10-11 23:22:22 +0300

    Button small fix

--- components/UI/Button/index.tsx
+++ components/UI/Button/index.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { Color } from '@/types/Color';
 import { useRouter } from 'next/router';
-import React from "react";
 import * as style from "./style.module.scss";
 
 export interface IButtonProps {
