1ec2fc79d561aa0fc5b00cbaad59745f9c43758f
Vladislav <vlad.zheltov1@gmail.com>
2021-10-16 23:24:53 +0300

    Text component update

---
+++ components/UI/Text/Text.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import classNames from "classnames";
+import style from "./Text.module.scss";
+
+export type TextModes =
+    | "div"
+    | "span"
+    | "label"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "p";
+
+export interface ITextProps {
+    /**
+     * Наполнители тега
+     */
+    children: any,
+
+    /**
+     * Вариация текста
+     */
+    mode?: TextModes,
+
+    /**
+     * Отображение текста - bold
+     */
+    bold?: boolean,
+
+    /**
+     * Отображение текста - italic
+     */
+    italic?: boolean,
+
+    /**
+     * Цвет текста
+     */
+    color?: string,
+
+    /**
+     * Размер текста
+     */
+    size?: string | number
+}
+
+export const Text: React.FC<ITextProps> = (props) => {
+
+    const {
+        children,
+        mode = "div",
+        bold,
+        italic,
+        color,
+        size,
+        ...restProps
+    } = props;
+
+    const className = classNames(
+        style.text,
+        {
+            [style.text_bold]: bold,
+            [style.text_italic]: italic
+        }
+    );
+
+    const extraStyles = {
+        color: color || null,
+        fontSize: size || null
+    }
+
+    const Mode = mode;
+
+    return (
+        <Mode className={className} style={extraStyles} {...restProps}>
+            {children}
+        </Mode>
+    )
+}
