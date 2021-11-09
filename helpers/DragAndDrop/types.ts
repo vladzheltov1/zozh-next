41430cbafa2eb7a3dd62eb6aa1d8715db190c733
Vladislav <vlad.zheltov1@gmail.com>
2021-11-09 21:51:42 +0300

    Quick type fix

---
+++ helpers/DragAndDrop/types.ts
@@ -0,0 +1,14 @@
+import { DraggableLocation } from "react-beautiful-dnd"
+
+type DragAndDropItems = {}
+
+type DragAndDropReorderList = {
+    item: DragAndDropItems,
+    source: DraggableLocation,
+    destination: DraggableLocation
+}
+
+export type {
+    DragAndDropItems,
+    DragAndDropReorderList
+}
