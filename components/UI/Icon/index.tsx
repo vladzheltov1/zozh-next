190576536c694abd2dac570e421897a274282842
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2021-11-12 22:33:12 +0300

    Init UI repo

--- components/UI/Icon/index.tsx
+++ components/UI/Icon/index.tsx
@@ -3,7 +3,6 @@ import style from "./style.module.scss";
 /**
  * @todo Доработать компонент
  */
-
 export const Icon = ({ icon }) => {
     const Icon = icon;
     return <span className={style.icon}><Icon /></span>
