c6432c4a22713abcfa75176123c624e3328944b8
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2021-11-28 13:25:24 +0300

    Card 1 Task 3 - init

---
+++ pages/card/1/Tasks/3/index.tsx
@@ -0,0 +1,14 @@
+import { Task } from "@/core/index";
+
+export const Task3 = () => {
+
+    const checkTask = () => { }
+
+    return (
+        <Task title="3. Заполните таблицу" action={checkTask}>
+            <>
+
+            </>
+        </Task>
+    )
+}
