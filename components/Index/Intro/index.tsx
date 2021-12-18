738901dfe1330a3e9d28c294c0400ac24272743e
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2021-12-18 22:13:03 +0300

    Quotation corrections

--- components/Index/Intro/index.tsx
+++ components/Index/Intro/index.tsx
@@ -17,10 +17,9 @@ export const IndexIntro = () => {
             {/* Quote */}
             <Text size={"1.3rem"} color={colors.gray0} italic>
                 <Text mode="span" color={colors.extraGreen}>&#171;</Text>
-                Единственный способ сохранить здоровье — это есть то, чего не хочешь, пить то, чего не любишь, и делать то, что не нравится.″
+                Единственный способ сохранить здоровье — это есть то, чего не хочешь, пить то, чего не любишь, и делать то, что не нравится
                 <Text mode="span" color={colors.extraGreen}>&#187;</Text>
-                {/* <Space height={"1rem"} /> */}
-                <Text color={colors.extraGreen}>-Марк Твен</Text>
+                <Text color={colors.extraGreen}>Марк Твен</Text>
                 <Space height={"2rem"} />
             </Text>
 
