59fb723ea195361105dde6a982ce05e08a0dfcb2
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-01-02 11:25:33 +0300

    Refactoring

--- pages/hub/index.tsx
+++ pages/hub/index.tsx
@@ -1,32 +1,22 @@
-import { Layout } from "@/layouts/Layout";
 import { Text } from "@/components/UI";
+import { Layout } from "@/layouts/Layout";
+import { Card } from "./Card";
 import style from "./style.module.scss";
-import { useRouter } from "next/router";
 
 const Hub = () => {
-
     const cards = [
-        { id: 1, title: "Основы Здорового Образа Жизни", link: "/card/1", picture: "vector-health-icon.jpg" },
-        { id: 2, title: "Мы есть то, что мы едим", link: "/card/2", picture: "healthy-food-vector.jpg" },
-        { id: 3, title: "Движение - жизнь", link: "/card/3", picture: "sport-balls-vectors.jpg" },
+        { title: "Основы Здорового Образа Жизни", link: "/card/1", picture: "vector-health-icon.jpg" },
+        { title: "Мы есть то, что мы едим", link: "/card/2", picture: "healthy-food-vector.jpg" },
+        { title: "Движение - жизнь", link: "/card/3", picture: "sport-balls-vectors.jpg" },
     ]
-
-    const router = useRouter();
-
-    const redirectHandler = (link) => {
-        router.replace(link);
-    }
 
     return (
         <Layout>
             <div className={style.hub__inner}>
                 <Text mode="h1">Здоровый образ жизни</Text>
                 <div className={style.card_wrapper}>
-                    {cards.map((card) => (
-                        <div onClick={() => redirectHandler(card.link)} key={card.id} className={style.card}>
-                            <img src={"/pictures/cards/" + card.picture} className={style.card__picture} />
-                            <Text bold className={style.card__title}>{card.title}</Text>
-                        </div>
+                    {cards.map((card, index) => (
+                        <Card key={index} data={card} />
                     ))}
                 </div>
             </div>
