210df3d9585fbef3396a5620b3f03868dc782244
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-01-08 15:56:15 +0300

    Refactoring

--- pages/card/1/Tasks/2/index.tsx
+++ pages/card/1/Tasks/2/index.tsx
@@ -34,85 +34,83 @@ export const Task2: FC = () => {
 
     return (
         <Task title={"2. Заполните пропуски, перетаскивая нужные слова"} action={checkTask}>
-            <>
+            <DragAndDropBlock onDragEnd={onDragEnd}>
                 <Space height={10} />
-                <DragAndDropBlock onDragEnd={onDragEnd}>
-                    <DropArea droppableId={ROOT_CONTAINER} outLook="root" direction="horizontal">
-                        {containers.rootContainer.map((item, index) => (
+                <DropArea droppableId={ROOT_CONTAINER} outLook="root" direction="horizontal">
+                    {containers.rootContainer.map((item, index) => (
+                        <DragItem
+                            key={item}
+                            draggableId={`task2Option${index}`}
+                            index={index}
+                        >
+                            {item}
+                        </DragItem>
+                    ))}
+                </DropArea>
+                <Space height={20} />
+                <Text size={"1.4rem"} className={style.task1Text}>
+                    <Text>Здоровый образ жизни -</Text>
+                    {/* Образ */}
+                    <DropArea droppableId={"gap1"} direction="horizontal" isDropDisabled={containers.gap1 != null}>
+                        {containers.gap1 ? (
                             <DragItem
-                                key={item}
-                                draggableId={`task2Option${index}`}
-                                index={index}
+                                draggableId={"gap1Value"}
+                                index={1}
                             >
-                                {item}
+                                {containers.gap1}
                             </DragItem>
-                        ))}
+                        ) : null}
                     </DropArea>
-                    <Space height={20} />
-                    <Text size={"1.4rem"} className={style.task1Text}>
-                        <Text>Здоровый образ жизни -</Text>
-                        {/* Образ */}
-                        <DropArea droppableId={"gap1"} direction="horizontal" isDropDisabled={containers.gap1 != null}>
-                            {containers.gap1 ? (
-                                <DragItem
-                                    draggableId={"gap1Value"}
-                                    index={1}
-                                >
-                                    {containers.gap1}
-                                </DragItem>
-                            ) : null}
-                        </DropArea>
-                        {/* Жизни */}
-                        <DropArea droppableId={"gap2"} direction="horizontal" isDropDisabled={containers.gap2 != null}>
-                            {containers.gap2 ? (
-                                <DragItem
-                                    draggableId={"gap2Value"}
-                                    index={2}
-                                >
-                                    {containers.gap2}
-                                </DragItem>
-                            ) : null}
-                        </DropArea>
-                        <Text>человека, направленный на</Text>
-                        {/* Укрепление */}
-                        <DropArea droppableId={"gap3"} direction="horizontal" isDropDisabled={containers.gap3 != null}>
-                            {containers.gap3 ? (
-                                <DragItem
-                                    draggableId={"gap3Value"}
-                                    index={3}
-                                >
-                                    {containers.gap3}
-                                </DragItem>
-                            ) : null}
-                        </DropArea>
-                        <Text>здоровья,</Text>
-                        {/* Профилактику */}
-                        <DropArea droppableId={"gap4"} direction="horizontal" isDropDisabled={containers.gap4 != null}>
-                            {containers.gap4 ? (
-                                <DragItem
-                                    draggableId={"gap4Value"}
-                                    index={4}
-                                >
-                                    {containers.gap4}
-                                </DragItem>
-                            ) : null}
-                        </DropArea>
-                        <Text>болезней и</Text>
-                        {/* Развитие */}
-                        <DropArea droppableId={"gap5"} direction="horizontal" isDropDisabled={containers.gap5 != null}>
-                            {containers.gap5 ? (
-                                <DragItem
-                                    draggableId={"gap5Value"}
-                                    index={5}
-                                >
-                                    {containers.gap5}
-                                </DragItem>
-                            ) : null}
-                        </DropArea>
-                        <Text>организма в целом.</Text>
-                    </Text>
-                </DragAndDropBlock>
-            </>
+                    {/* Жизни */}
+                    <DropArea droppableId={"gap2"} direction="horizontal" isDropDisabled={containers.gap2 != null}>
+                        {containers.gap2 ? (
+                            <DragItem
+                                draggableId={"gap2Value"}
+                                index={2}
+                            >
+                                {containers.gap2}
+                            </DragItem>
+                        ) : null}
+                    </DropArea>
+                    <Text>человека, направленный на</Text>
+                    {/* Укрепление */}
+                    <DropArea droppableId={"gap3"} direction="horizontal" isDropDisabled={containers.gap3 != null}>
+                        {containers.gap3 ? (
+                            <DragItem
+                                draggableId={"gap3Value"}
+                                index={3}
+                            >
+                                {containers.gap3}
+                            </DragItem>
+                        ) : null}
+                    </DropArea>
+                    <Text>здоровья,</Text>
+                    {/* Профилактику */}
+                    <DropArea droppableId={"gap4"} direction="horizontal" isDropDisabled={containers.gap4 != null}>
+                        {containers.gap4 ? (
+                            <DragItem
+                                draggableId={"gap4Value"}
+                                index={4}
+                            >
+                                {containers.gap4}
+                            </DragItem>
+                        ) : null}
+                    </DropArea>
+                    <Text>болезней и</Text>
+                    {/* Развитие */}
+                    <DropArea droppableId={"gap5"} direction="horizontal" isDropDisabled={containers.gap5 != null}>
+                        {containers.gap5 ? (
+                            <DragItem
+                                draggableId={"gap5Value"}
+                                index={5}
+                            >
+                                {containers.gap5}
+                            </DragItem>
+                        ) : null}
+                    </DropArea>
+                    <Text>организма в целом.</Text>
+                </Text>
+            </DragAndDropBlock>
         </Task>
     )
 }
