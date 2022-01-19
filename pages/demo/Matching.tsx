fb7b3c8c78058d577b150d153d0055429a6a057d
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-01-19 18:34:29 +0300

    Refactoring

--- pages/demo/Matching.tsx
+++ pages/demo/Matching.tsx
@@ -1,20 +1,20 @@
-import { FC, useState } from "react";
-import { Matcher } from "@/libs/Matcher";
 import { Space, Text } from "@/components/UI";
+import { Matcher } from "@/libs/Matcher";
+import { FC } from "react";
+
+const lists = {
+    left: ["Правильное", "Утренняя"],
+    right: ["пробежка", "питание"]
+}
 
 export const Matching: FC = () => {
-    const [lists, setLists] = useState({
-        left: ["Правильное", "Утренняя"],
-        right: ["пробежка", "питание"]
-    });
-
     return (
         <>
             <Text bold size={21}>
                 Составьте осмысленные пары слов
             </Text>
             <Space height={10} />
-            <Matcher leftList={lists.left} rightList={lists.right} />
+            <Matcher leftList={lists.left} rightList={lists.right} onFinish={() => { }} />
         </>
     )
 }
