e0c1a9c6be2c9c7ed8e5e5b95b2d2b0a950addd7
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-01-19 18:43:12 +0300

    Finish: Card 1

--- pages/card/1/Tasks/5/index.tsx
+++ pages/card/1/Tasks/5/index.tsx
@@ -1,33 +1,52 @@
-import { Task } from "@/core/index";
+import { onAnswerSubmit, Task } from "@/core/index";
 import { Matcher } from "@/libs/Matcher";
+import { useState } from "react";
+
+const lists = {
+    left: ["Правильное", "Силовые", "Активный", "Занятие", "Диета"],
+    right: ["спортом", "тренировки", "питание", "для похудения", "образ жизни"]
+}
+
+const correctPairs = {
+    "Правильное": "питание",
+    "Силовые": "тренировки",
+    "Активный": "образ жизни",
+    "Занятие": "спортом",
+    "Диета": "для похудения"
+}
+
+const CORRECT_PAIRS_LENGTH = 5;
 
 export const Task5 = () => {
+    const [pairs, setPairs] = useState(null);
 
-    const leftList = [
-        "Правильное",
-        "Силовые",
-        "Активный",
-        "Занятие",
-        "Диета"
-    ]
-
-    const rightList = [
-        "спортом",
-        "тренировки",
-        "питание",
-        "для похудения",
-        "образ жизни",
-    ]
+    const getPairs = (updatedPairs) => {
+        setPairs(updatedPairs);
+    }
 
     const handleCheck = () => {
+        const isCorrect = checkIfPairsMatch();
+        onAnswerSubmit(isCorrect);
+    }
 
+    const checkIfPairsMatch = (): boolean => {
+        if (!pairs) return false;
+
+        let isCorrect = true;
+
+        pairs.map((pair) => {
+            if (pair.right != correctPairs[pair.left]) {
+                isCorrect = false;
+                return;
+            }
+        })
+
+        return isCorrect;
     }
 
     return (
         <Task title="5. Соотнесите слова, чтобы получились правильные словосочетания." action={handleCheck}>
-            <Matcher leftList={leftList} rightList={rightList}>
-
-            </Matcher>
+            <Matcher leftList={lists.left} rightList={lists.right} onFinish={getPairs} />
         </Task>
     )
 }
