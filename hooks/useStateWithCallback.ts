4b5dd04ca73be6433565e7f5555b8b297847d217
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-01-29 13:46:18 +0300

    Refactoring

--- hooks/useStateWithCallback.ts
+++ hooks/useStateWithCallback.ts
@@ -2,8 +2,8 @@ import { useEffect, useRef, useState } from "react";
 
 /**
  * @todo починить баг: обновление состояния внутри хука не приводит в обновлению компонента, где этот хук используется
+ * @description на данный момент требуется в `/pages/card/1/Tasks/5`
  * @see https://ru.stackoverflow.com/questions/1371961/%d0%a5%d1%83%d0%ba-usestate-%d1%81-callback%d0%be%d0%bc
- * @deprecated
  */
 export const useStateWithCallback = (initialState) => {
     const [state, setState] = useState(initialState);
