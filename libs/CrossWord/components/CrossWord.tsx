d4b635fbfda1ddb8d36662fc6b9f3148fb029526
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-02-07 12:26:38 +0300

    Refactoring

--- libs/CrossWord/components/CrossWord.tsx
+++ libs/CrossWord/components/CrossWord.tsx
@@ -1,5 +1,5 @@
 import { Text } from "@/components/UI";
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import crossWordStyle from "../styles/CrossWord.module.scss";
 
 export interface ICrossWord {
@@ -7,7 +7,7 @@ export interface ICrossWord {
     letters: number
 }
 
-const regex = /[А-Яа-я()]/;
+const regex = /[А-Яа-я]/;
 
 export const CrossWord: FC<ICrossWord> = (props) => {
     const { question, letters } = props;
@@ -15,42 +15,35 @@ export const CrossWord: FC<ICrossWord> = (props) => {
     const [gaps, setGaps] = useState([]);
 
     const onChange = (event) => {
-        // event.preventDefault();
+        event.preventDefault();
+
+        if (!regex.test(event.target.value) && event.key !== "Backspace") return;
 
         const form = event.target.form;
         const index = [...form].indexOf(event.target);
 
         const offset = event.key === "Backspace" ? -1 : 1;
+
+        setGaps(
+            gaps.map((gap, i) => {
+                if (event.key === "Backspace") {
+                    gap = "";
+                } else if (i === index) {
+                    gap = event.target.value;
+                }
+                console.log(`${i} set to ${gap}`)
+                return gap;
+            })
+        );
+
         const focusElement = form.elements[index + offset];
         if (!focusElement) return;
         form.elements[index + offset].focus();
-
-        if (regex.test(event.target.value)) {
-            console.log("Write");
-        } else if (event.key === "Backspace") {
-            console.log("Erase");
-        } else {
-            console.log("Do nothing");
-        }
-
-
-        // if (regex.test(event.target.value) || event.key === "Backspace") {
-        //     // setGaps(
-        //     //     gaps.map((gap, i) => {
-        //     //         if (event.key === "Backspace") {
-        //     //             gap = "";
-        //     //         } else if (i === index) {
-        //     //             gap = event.target.value;
-        //     //         }
-        //     //         return gap;
-        //     //     })
-        //     // );
-        // }
     }
 
-    useEffect(() => {
-        console.log(gaps);
-    }, [gaps])
+    // useEffect(() => {
+    //     console.log(gaps);
+    // }, [gaps])
 
     return (
         <div className={crossWordStyle.crossWordWrapper}>
@@ -74,7 +67,7 @@ interface ILetterProps {
 }
 
 const Letter: FC<ILetterProps> = (props) => {
-    const { onChange = () => { }, value } = props;
+    const { onChange = () => { }, value = "" } = props;
     return <input
         className={crossWordStyle.letter}
         maxLength={1}
