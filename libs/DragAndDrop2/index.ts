00eaaf119781b71e658e77e7449851d4c06de650
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-02-13 22:04:26 +0300

    Deprecate old features

--- libs/DragAndDrop2/index.ts
+++ libs/DragAndDrop2/index.ts
@@ -5,4 +5,7 @@ export * from "./scripts/DragAndDrop";
 export { TextWithGaps } from "./styles/";
 export * from "./types";
 
+/**
+ * @deprecated no need to use it anymore :)
+ */
 export const ROOT_CONTAINER = "rootContainer";
