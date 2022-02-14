b122e84824a042cc59b2a6ff546ec822b2fd1a48
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-02-14 22:43:57 +0300

    Testing

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,9 +1,10 @@
+import { Text } from "@/components/UI";
 import { Layout } from "@/layouts/Layout";
 
 const Test = () => {
     return <Layout>
         <div className="wrapper">
-            Test page
+            <Text mode="h1">Тестовая страница</Text>
         </div>
     </Layout>
 }
