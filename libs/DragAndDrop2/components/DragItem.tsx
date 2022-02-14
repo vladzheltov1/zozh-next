6d841b16189f38b3c0bd40ab144650943faecf27
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-02-14 22:42:58 +0300

    Refactoring + found a bug

--- libs/DragAndDrop2/components/DragItem.tsx
+++ libs/DragAndDrop2/components/DragItem.tsx
@@ -1,6 +1,6 @@
 import { random } from "@/libs/Random";
 import classNames from "classnames";
-import { CSSProperties, FC } from "react";
+import { FC } from "react";
 import { Draggable } from 'react-beautiful-dnd';
 import { dragItemStyles } from "../styles";
 
@@ -12,16 +12,22 @@ export interface IDragItemProps {
     index?: number,
 }
 
+// FIXME: При обновлении страницы появляются ошибки о том, что `draggbleId` на клиенте не сходится с сервером.
+// Вследствие этого перестаёт работать система.
 export const DragItem: FC<IDragItemProps> = (props) => {
     const { children, isDragDisabled, draggableId, className, index = 0 } = props;
 
     const classNameList = classNames({
         [dragItemStyles.dragItem]: true,
         [className]: className
-    })
+    });
+
+    const getDraggableId = () => {
+        return `draggable-${random.getInt(0, 10000)}`;
+    }
 
     return (
-        <Draggable draggableId={draggableId || `draggable-${random.getInt(0, 10000)}`} index={index} isDragDisabled={isDragDisabled}>
+        <Draggable draggableId={draggableId || getDraggableId()} index={index} isDragDisabled={isDragDisabled}>
             {provided => (
                 <div
                     className={classNameList}
